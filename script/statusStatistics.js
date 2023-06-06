2fd99c9882b1448e39e1d61ad74f7eb402dbc9fc
SNVangelov22 <SNVangelov22@codingburgas.bg>
2023-06-06 19:03:03 +0300

    Scroll spy + JS fixes and final fixes

---
+++ script/statusStatistics.js
@@ -0,0 +1,23 @@
+const eventDate = new Date('2019-02-10T23:00:00');
+function updateCounter() {
+    const currentTime = new Date();
+
+    const timeDiff = currentTime.getTime() - eventDate.getTime();
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    document.getElementById("clock").innerHTML = (`Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`);
+    
+    let velocity = 90;
+
+    let distance = timeDiff/1000 * velocity;
+    document.getElementById("distance").innerHTML = `${Math.round(distance)} km`; 
+
+    let oneway = distance / 299792;
+    document.getElementById("oneWay").innerHTML = `${Math.round(oneway/60/60)} Light hours`;
+}
+
+setInterval(updateCounter, 1000);
